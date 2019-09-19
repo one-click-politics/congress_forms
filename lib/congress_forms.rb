c67302f91def469e507d534ec51b7c800b77e4a2
= <jessethebuilder@yahoo.com>
2019-09-18 19:30:14 -0700

    mitm

--- lib/congress_forms.rb
+++ lib/congress_forms.rb
@@ -23,8 +23,14 @@ unless ENV["CWC_API_KEY"].nil?
 end
 
 
+# Capybara.register_driver :chrome do
+#   Capybara::Selenium::Driver.new(nil, browser: :chrome)
+# end
+
+# ENV['HEADLESS'] = '0'
+
 Capybara.register_driver :chrome do
-  Capybara::Selenium::Driver.new(nil, browser: :chrome)
+  Capybara::Selenium::Driver.new(nil, browser: :chrome, options: browser_options)
 end
 
 Capybara.register_driver :remote do
@@ -56,10 +62,24 @@ end
 #   Capybara::Selenium::Driver.new(app, :profile => profile)
 # end
 
+# NOTES -------------------------------------------------------------------------------------------------
+
+# Go to https://mitmproxy.org/ and download the binarys. Extract them to /~ or
+# wherever makes sense for the deployment.
+
+# Run the following command to start a MITM server (assuming mitmdump was extracted to /~) 
+# ~/mitmdump -p 9000 --mode upstream:http://proxy.crawlera.com:8010 --set upstream_auth=dc5106a9eee74ef8896bbe4599e4c332: --ssl-insecure
+
+# If port 9000 is not available, modify the above command. You will also need
+# to change the line below for proxy_server
+
+# end NOTES ---------------------------------------------------------------------------------------------
+
 Capybara.register_driver :headless_chrome do |app|
   Capybara::Selenium::Driver.load_selenium
   browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
     opts.args.merge(%w(--new-window --no-sandbox --disable-dev-shm-usage  --window-size=1200,1400))
+    opts.args << '--proxy-server=http://localhost:9000'
     opts.args << '--headless' unless ENV["HEADLESS"] == "0"
     opts.args << '--disable-gpu' if Gem.win_platform?
   end
@@ -77,6 +97,7 @@ end
 #   #   :http => "proxy.crawlera.com:8010",
 #   #   :ssl => "proxy.crawlera.com:8010")
 #
+
 #   proxy = Selenium::WebDriver::Proxy.new(:http => "http://dc5106a9eee74ef8896bbe4599e4c332:@proxy.crawlera.com:8010")
 #   caps = Selenium::WebDriver::Remote::Capabilities.chrome(:proxy => proxy)
 #
--- lib/congress_forms/version.rb
+++ lib/congress_forms/version.rb
@@ -1,3 +1,4 @@
 module CongressForms
-  VERSION = "0.1.15"
+  # VERSION = "0.1.15"
+  VERSION = "0.1.42"
 end
